df7320ea2d5709a8f57d7316b59d1716bfc14dbd
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-09-05 01:26:55 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,6 +12,7 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!" if age <= 12
+    if age <= 12
+      puts "Happy Birthday #{kids_name}! You are now #{age} years 
   end
 end
